6290ce861253340442684803001b14c5a15eb916
Jonathan Parker <jonp@roadhouse.com.au>
2021-06-22 23:37:53 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fetchData = () => {
+	return axios.get("https://randomuser.me/api/?results=20")
 }
 
-export default App;
+const getSortAscCompareFunction = (sortColumn) => (a, b) =>
+	a[sortColumn] > b[sortColumn] ? 1 : a[sortColumn] < b[sortColumn] ? -1 : 0
+
+const getSortDescCompareFunction = (sortColumn) => (a, b) =>
+	-getSortAscCompareFunction(sortColumn)(a, b)
+
+const getCompareFunction = (sortDirection, sortColumn) => {
+	if (sortDirection === "none") {
+		return (a, b) => 0
+	}
+	if (sortDirection === "asc") {
+		return getSortAscCompareFunction(sortColumn)
+	}
+	return getSortDescCompareFunction(sortColumn)
+}
+
+const sortLocations = (locations, sort) => {
+	locations.sort(getCompareFunction(sort.direction, sort.column))
+}
+
+function App() {
+	const [locations, setLocations] = useState([])
+	const [headers, setHeaders] = useState([])
+	const [sort, setSort] = useState({ direction: "none", column: "" })
+	const [filter, setFilter] = useState("")
+
+	useEffect(() => {
+		fetchData().then((result) => {
+			const { results } = result.data
+			const flattenedLocations = results.map(({ location }) => ({
+				"Street No": location.street.number,
+				"Street Name": location.street.name,
+				city: location.city,
+				state: location.state,
+				country: location.country,
+				"Post Code": location.postcode,
+				latitude: location.coordinates.latitude,
+				longitude: location.coordinates.longitude,
+				"Timezone Offset": location.timezone.offset,
+				"Timezone Description": location.timezone.description,
+			}))
+
+			const headers = Object.keys(flattenedLocations[0])
+			setHeaders(headers)
+			const filteredLocations = flattenedLocations.filter((loc) =>
+				headers.some((header) => loc[header].toString().search(filter) > -1)
+			)
+			if (sort.direction !== "none") {
+				sortLocations(filteredLocations, sort)
+			}
+			setLocations(filteredLocations)
+		})
+	}, [sort, filter])
+
+	return (
+		<div className="App">
+			<div>
+				<h1>Locations </h1>
+				<label htmlFor="filter">Find:</label>
+				<input
+					id="filter"
+					type="text"
+					value={filter}
+					onChange={(e) => setFilter(e.target.value)}
+				/>
+				<div>
+					<table>
+						<thead>
+							<tr>
+								{headers.map((header, headerIdx) => (
+									<th
+										key={headerIdx}
+										onClick={(_) => {
+											setSort({
+												direction: sort.direction !== "asc" ? "asc" : "desc",
+												column: header,
+											})
+										}}
+									>
+										{header}
+									</th>
+								))}
+							</tr>
+						</thead>
+						<tbody>
+							{locations.length > 0 ? (
+								locations.map((location, locationIdx) => (
+									<tr key={locationIdx}>
+										{headers.map((header, headerIdx) => (
+											<td key={headerIdx}>{location[header]}</td>
+										))}
+									</tr>
+								))
+							) : (
+								<tr>
+									<td colSpan={headers.length}>No results found</td>
+								</tr>
+							)}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default App
